0a6a0b335762ba084829528e11134612b3e81947
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:14:36 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,8 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.leaflet.LeafletPageConfigurator;
-
 module com.jwebmp.plugins.leaflet {
 
 	exports com.jwebmp.plugins.leaflet;
@@ -17,9 +12,9 @@ module com.jwebmp.plugins.leaflet {
 	requires java.logging;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with LeafletPageConfigurator;
-	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.leaflet.implementations.LeafletExclusionsModule;
-	provides IGuiceScanJarExclusions with com.jwebmp.plugins.leaflet.implementations.LeafletExclusionsModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.leaflet.LeafletPageConfigurator;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.leaflet.implementations.LeafletExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.leaflet.implementations.LeafletExclusionsModule;
 
 	opens com.jwebmp.plugins.leaflet to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.leaflet.options to com.fasterxml.jackson.databind, com.jwebmp.core;
