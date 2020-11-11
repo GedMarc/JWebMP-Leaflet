fb29afb1cd9487f69e5e7eaaca3003129076e00c
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:19:50 +0200

    Jakarta Namespace - JLink building

--- src/main/java/com/jwebmp/plugins/leaflet/LeafletPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/leaflet/LeafletPageConfigurator.java
@@ -5,7 +5,7 @@ import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.plugins.jquery.JQueryPageConfigurator;
 import com.jwebmp.core.services.IPageConfigurator;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 /**
  * Configures the Bootstrap Page Configurator
